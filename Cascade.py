4ce7dfea6dd111d619b1773c8c6bf1e6126a4ced
SeanPaulLevesque <41477212+SeanPaulLevesque@users.noreply.github.com>
2019-08-14 10:51:58 -0400

    Update

--- Cascade.py
+++ Cascade.py
@@ -3,12 +3,12 @@ import skrf as rf
 
 folder_name = "s2p"
 
+#Plot each s2p file on the same graph
 for filename in os.listdir(folder_name):
-    #Plot each s2p file on the same graph
     ntwk = rf.Network(folder_name + '\\' + filename)
-    Mag = ntwk['2ghz'].s21.s_db[0][0][0]
+    # Mag = ntwk['2ghz'].s21.s_db[0][0][0]
     kwargs = {"marker": "o"}
-    ntwk.plot_s_db(m=0, n=1, label=filename[:-4] + " " + str(round(Mag, 3)) + "dB", **kwargs)
+    ntwk.plot_s_db(m=0, n=1, label=filename[:-4], **kwargs)
     loop = 'done'
 
 done = 0
