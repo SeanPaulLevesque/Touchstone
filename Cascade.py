03ae436e7ff100141950cd0c4f650d414c91b862
SeanPaulLevesque <41477212+SeanPaulLevesque@users.noreply.github.com>
2019-08-14 13:31:10 -0400

    Update

--- Cascade.py
+++ Cascade.py
@@ -4,17 +4,20 @@ from matplotlib import pyplot as plt
 from matplotlib import style
 
 folder_name = "s2p"
-with style.context('seaborn-ticks'):
-    plt.title('Smith Chart With Legend Room');
+freq_s = '3.6ghz'
+freq_v = float(freq_s[:-3]) * 1e9
 
 #Plot each s2p file on the same graph
 for filename in os.listdir(folder_name):
     ntwk = rf.Network(folder_name + '\\' + filename)
-    Mag = ntwk['2ghz'].s21.s_db[0][0][0]
+    MagPoint = ntwk[freq_s]
+    MagVal = ntwk[freq_s].s21.s_db[0][0][0]
     ntwk.plot_s_db(m=0, n=1, label=filename[:-4])
-    with style.context('seaborn-ticks'):
-        plt.text(2e9, Mag, str(round(Mag, 3)));
 
+    # Mag1.plot_s_db(m=0, n=1, marker = 'o')
+    with style.context('seaborn-ticks'):
+        plt.title('ADRF5545 AH Board Trace Loss');
+        plt.text(freq_v, MagVal, str(round(MagVal, 3)));
 
     loop = 'done'
 
